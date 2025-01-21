90640ec9ec6890df9114de7e706b77c86dc3cdc5
Shellvi-anna <shellviana.47@smk.belajar.id>
2025-01-21 09:44:25 +0700

    register proses

--- auth/register_process.php
+++ auth/register_process.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $name = $_POST["name"];
     $password = $_POST["password"]
-    $hashedPassword = password_hash($_password, PASSWORD_DEFAULT);
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
     $sql = "INSERT INTO users (username, name, password)
            VALUES ('$username', '$name', '$hashedPassword')";
